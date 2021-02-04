e24ce3cebadb40fab2ef3dbf3e3087ce8bc53309
Mateus Med <mateus.medeiros46@gmail.com>
2021-02-04 19:57:54 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import TopNavBar from "./components/TopNavBar";
+import Page1 from "./pages/Page1"
+import Page2 from "./pages/Page2"
+import MyDataTable from "./pages/datatable/MyDataTable"
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <TopNavBar />
+
+          <Switch>
+              <Route path={'/'} exact component={Page1}/>
+              <Route path={'/home'} exact component={Page1}/>
+              <Route path={'/link'} exact component={Page2}/>
+              <Route path={'/datatable'} exact component={MyDataTable}/>
+          </Switch>
+      </Router>
   );
 }
 
---
+++ src/pages/Page2.js
@@ -0,0 +1,13 @@
+
+
+
+function Page2() {
+
+    return (
+        <div>
+            <h1> Pagina 2</h1>
+        </div>
+    );
+}
+
+export default Page2;
